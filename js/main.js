17a9bcf99a169c18ef35ea912de903b1755b21f5
kladya <konkla@gmail.com>
2020-12-04 01:08:06 +0100

    Some changes

--- js/main.js
+++ js/main.js
@@ -6,6 +6,6 @@ const getTimeNow = dateNow => {
     dateNow = new Date();
     const timeNow = dateNow.toLocaleTimeString('hu');
     display.textContent = timeNow;
-};
+}
 
 setInterval(getTimeNow, 1000);
